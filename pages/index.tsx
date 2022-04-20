467ab6e12aaa1cb9261fcf9885d0f43430343bef
plesio <qq.plesio@gmail.com>
2022-04-20 13:45:05 +0900

    add title

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,19 @@
 // noinspection JSUnusedGlobalSymbols
 
 import type { NextPage } from "next";
+import Head from "next/head";
 import MainView from "../src/feature/MainView";
 
-
 const Index: NextPage = () => {
-  return <MainView />;
+  return (
+    <>
+      <Head>
+        <title>ばぶっち</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
+      <MainView />
+    </>
+  );
 };
 
 export default Index;
